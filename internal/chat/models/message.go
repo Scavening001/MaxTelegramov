fd63163c9f7e86ae05059c9681eed266aac1c94c
birthtree <vladimirakkuzin10@gmail.com>
2026-04-13 22:11:23 +0300

    Added model of Message

--- internal/chat/models/message.go
+++ internal/chat/models/message.go
@@ -1,1 +1,10 @@
 package models
+
+import "time"
+
+type Message struct {
+	Type     string    `json:"type"`
+	Username string    `json:"username"`
+	Text     string    `json:"text"`
+	Time     time.Time `json:"time"`
+}
